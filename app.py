784b6785b90b0b74524cbe90f9d5168d84f97e39
ruizcarr <jaimeruizcarrillo@gmail.com>
2025-12-16 16:27:40 +0100

    Merge remote-tracking branch 'origin/main'

--- app.py
+++ app.py
@@ -245,6 +245,7 @@ def display_portfolio_positions(eod_log_history,trading_history,date,settings,re
         value=int(last_portfolio[j])
         delta=int(last_trade[j])
         cols[i].metric(label=label,value=value,delta=delta)
+        st.subheader(f"{pos_value_today_eur:,.0f} € ")
 
         #Chart weights evolution
 
