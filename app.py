e69fae5e588ef48100f06249e4614722a7d8c1f5
ruizcarr <jaimeruizcarrillo@gmail.com>
2025-12-22 12:29:30 +0100

    Merge remote-tracking branch 'origin/main'

--- app.py
+++ app.py
@@ -365,12 +365,12 @@ def display_tickers_data(closes, returns, settings, sidebar=False, daysback=3*22
             if "last_refresh" not in st.session_state:
                 st.session_state["last_refresh"] = pd.Timestamp.now(tz="Europe/Madrid")
 
+            cols[0].checkbox('Show Annalytics:', value=None, key='qstats')
+
             # ----------------- Refresh Button -----------------
             if cols[0].button("🔄 Refresh Data"):
                 st.cache_data.clear()
                 st.cache_resource.clear()
-                if cols[0].checkbox('Show Annalytics:', value=None, key='qstats'):
-                    st.session_state.qstats = True
                 st.session_state["last_refresh"] = pd.Timestamp.now(tz="Europe/Madrid")
                 st.rerun() # <--- Force instant rerun
 
