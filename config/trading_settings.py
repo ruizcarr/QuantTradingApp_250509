09bb2eea043e83ad0aaea2fb9af5d818772590de
ruizcarr <jaimeruizcarrillo@gmail.com>
2026-03-24 12:38:25 +0100

    Merge remote-tracking branch 'origin/main'

--- config/trading_settings.py
+++ config/trading_settings.py
@@ -12,5 +12,5 @@ settings =retrieve_training_settings()
 settings['trading_app_only']=True
 settings['start'] = '2017-01-01' #'2020-01-01' # '2019-01-01'
 settings['end'] =(date.today() + timedelta(days=1)).isoformat()
-settings['startcash'] = 95000 #200000#240000 #47000 #56500#210000#35000 #30650 #27800 #54300 #52300 # #EUR
-
+settings['startcash'] = 110000 #95000 2017#200000#240000 #47000 #56500#210000#35000 #30650 #27800 #54300 #52300 # #EUR
+settings['max_n_contracts'] = 6
